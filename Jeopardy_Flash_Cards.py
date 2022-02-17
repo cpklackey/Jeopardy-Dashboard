c5c5f8d26c7ebac6c181ac94e3bea75fb5c4fdf2
cpklackey <88690538+cpklackey@users.noreply.github.com>
2022-02-17 09:35:50 -0800

    added slider

--- Jeopardy_Flash_Cards.py
+++ Jeopardy_Flash_Cards.py
@@ -4,18 +4,26 @@ import numpy as np
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-  
-data =  pd.read_csv('jp_keywords.csv')
 
+data =  pd.read_csv('jp_keywords.csv')
+data.drop_duplicates(subset=['answer'],inplace = True)
 data.set_index('answer', inplace = True)
 st.sidebar.header("Word Cloud Generator")
-sample_list = data.index.sort_values()
+sample_list = list(data.index.sort_values())
 ##st.session_state.sample_list = sample_list
-answer = st.sidebar.selectbox('Display Word Cloud for which Jeopardy Answer?', sample_list)
+##start = '0'
+##end = 'zurich'
+start, end = st.sidebar.select_slider('Select a range of answers',
+options=sample_list
+,value=('0', 'zurich'),
+format_func=lambda x: x[0]
+)
+answer = st.sidebar.selectbox('Display Word Cloud for which Jeopardy Answer?', sample_list[sample_list.index(start):sample_list.index(end)])
 ##st.sidebar.button('Refresh List')
 if not st.sidebar.checkbox("Close", True, key='3'):
+    
  
-    st.subheader('Word Cloud for %s' % (answer))
+    st.subheader('Word Cloud for "%s"' % (answer))
     
     processed_words = data[data.index == answer]['keywords'][0]
   
